9ade3fd68a09c5893406df1b2edbcf06a1e959fa
togi-hassaku-sairei <togi.hassaku.sairei@gmail.com>
2026-09-09 23:54:37 +0900

    Update map.js

--- js/map.js
+++ js/map.js
@@ -17,9 +17,8 @@ map.on("click", function(){
 
 /* ベースマップ（標準地図＝OpenStreetMap／航空写真＝国土地理院シームレス空中写真） */
 const baseLayers = {
-  std: L.tileLayer("https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png", {
-    maxNativeZoom: 18, maxZoom: 19,
-    attribution: '<a href="https://maps.gsi.go.jp/development/ichiran.html" target="_blank" rel="noopener">地理院タイル（淡色地図）</a>'
+  std: L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19, attribution: "&copy; OpenStreetMap contributors"
   }),
   photo: L.tileLayer("https://cyberjapandata.gsi.go.jp/xyz/seamlessphoto/{z}/{x}/{y}.jpg", {
     maxNativeZoom: 18, maxZoom: 19,
